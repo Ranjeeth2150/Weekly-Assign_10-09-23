d3a6a1032a630a8816633904dd064b5dbc208be7
Jangam Ranjeeth <jangamr.jr@gmail.com>
2023-09-10 23:34:02 +0530

    Initial commit

---
+++ src/main/java/org/example/mail/MailConstants.java
@@ -0,0 +1,6 @@
+package org.example.mail;
+
+public class MailConstants {
+    public static final String SENDER = "jangamr.jr@gmail.com";
+    public static final String APP_PASSWORD = "kpszskzrjaznfsua";
+}
